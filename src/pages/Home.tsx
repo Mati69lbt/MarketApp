d3f715ad442f7d405e11ac537e5bfbdee9530d8c
paranaSorteo <paranasorteo@gmail.com>
2025-07-30 09:25:48 -0300

    021.3

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -170,7 +170,7 @@ function Home() {
       )}
 
       <div className="footer">
-        <p>&copy; {new Date().getFullYear()} - MDelgado (JUL) - V21.2</p>
+        <p>&copy; {new Date().getFullYear()} - MDelgado (JUL) - V21.3</p>
       </div>
     </div>
   );
